2267ed3ccc400a3c2953a3b41359132705894b8d
jaber4770 <jaber.ahmed4770@gmail.com>
2022-02-04 18:41:49 +0600

    uptadet

---
+++ src/Pages/Home/Services/Services.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+
+const object = [
+    {name:'Car Reapire', price: 2000, time: 2, img: 'https://ibb.co/Cz4h1Yg'},
+    {name:'Car Reapire', price: 2000, time: 2, img: 'https://ibb.co/B2yfRrt'},
+    {name:'Car Reapire', price: 2000, time: 2, img: 'https://ibb.co/nwV71SN'},
+    {name:'Car Reapire', price: 2000, time: 2, img: 'https://ibb.co/C11LdLR'},
+    {name:'Car Reapire', price: 2000, time: 2, img: 'https://ibb.co/y4Fm6pg'},
+    {name:'Car Reapire', price: 2000, time: 2, img: 'https://ibb.co/t4n2Ncc'},
+    {name:'Car Reapire', price: 2000, time: 2, img: 'https://ibb.co/mJMLmck'},
+    {name:'Car Reapire', price: 2000, time: 2, img: 'https://ibb.co/B2yfRrt'}
+]
+
+
+
+const Services = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Services;
