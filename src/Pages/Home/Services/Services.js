98f7e9518743a620635ecebecb9cc46275f16182
jaber4770 <jaber.ahmed4770@gmail.com>
2022-02-04 19:02:36 +0600

    photo usr set

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,15 +1,24 @@
 import React from 'react';
 
 
+
+
+
+
+
+const banner = "https://ibb.co/J37YYF0";
+const favlogo = 'https://ibb.co/0yn2QjQ';
+
+
 const object = [
-    {name:'Car Reapire', price: 2000, time: 2, img: 'https://ibb.co/Cz4h1Yg'},
-    {name:'Car Reapire', price: 2000, time: 2, img: 'https://ibb.co/B2yfRrt'},
-    {name:'Car Reapire', price: 2000, time: 2, img: 'https://ibb.co/nwV71SN'},
-    {name:'Car Reapire', price: 2000, time: 2, img: 'https://ibb.co/C11LdLR'},
-    {name:'Car Reapire', price: 2000, time: 2, img: 'https://ibb.co/y4Fm6pg'},
-    {name:'Car Reapire', price: 2000, time: 2, img: 'https://ibb.co/t4n2Ncc'},
-    {name:'Car Reapire', price: 2000, time: 2, img: 'https://ibb.co/mJMLmck'},
-    {name:'Car Reapire', price: 2000, time: 2, img: 'https://ibb.co/B2yfRrt'}
+    { name: 'Car Reapire', price: 2000, time: 2, img: 'https://ibb.co/Cz4h1Yg' },
+    { name: 'Car Reapire', price: 2000, time: 2, img: 'https://ibb.co/B2yfRrt' },
+    { name: 'Car Reapire', price: 2000, time: 2, img: 'https://ibb.co/nwV71SN' },
+    { name: 'Car Reapire', price: 2000, time: 2, img: 'https://ibb.co/y4Fm6pg' },
+    { name: 'Car Reapire', price: 2000, time: 2, img: 'https://ibb.co/C11LdLR' },
+    { name: 'Car Reapire', price: 2000, time: 2, img: 'https://ibb.co/ZhwYBwj' },
+    { name: 'Car Reapire', price: 2000, time: 2, img: 'https://ibb.co/t4n2Ncc' },
+    { name: 'Car Reapire', price: 2000, time: 2, img: 'https://ibb.co/mJMLmck' }
 ]
 
 
@@ -17,7 +26,7 @@ const object = [
 const Services = () => {
     return (
         <div>
-            
+
         </div>
     );
 };
