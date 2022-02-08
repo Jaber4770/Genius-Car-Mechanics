0bd6eebfa86d96dffcfe9cc2125eaa1b579dd665
jaber4770 <jaber.ahmed4770@gmail.com>
2022-02-08 22:51:37 +0600

    initializeAuthentication called on the useFirebas page

--- src/Hooks/useFirebase.js
+++ src/Hooks/useFirebase.js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react';
 import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged, signOut } from "firebase/auth";
+import initializeAuthentication from '../Pages/Login/Firebase/firebase.init';
+initializeAuthentication();
 
 const useFirebase = () => {
     const [users, setUsers] = useState({});
