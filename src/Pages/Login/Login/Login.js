deaa3ea39330acf86e7693af3d41d01b2abcaaf3
jaber4770 <jaber.ahmed4770@gmail.com>
2022-02-08 23:00:00 +0600

    logout and login with google added on butoon by onClick

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import useAuth from '../../../Hooks/useAuth';
 
 const Login = () => {
+    const { SignInUsingGoogle } = useAuth();
     return (
         <div>
             <h1>Login your account!</h1>
             <h3>Login only availabe by Google</h3>
-            <button type="button" class="btn btn-outline-success">Google</button>
+            <button onClick={SignInUsingGoogle} type="button" class="btn btn-outline-success">Google</button>
         </div>
     );
 };
