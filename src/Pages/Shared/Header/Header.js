340ec0e997b674f07fb6006e49dca1a028d3820b
jaber4770 <jaber.ahmed4770@gmail.com>
2022-02-08 18:58:00 +0600

    navbar added and maked responsive

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -6,19 +6,18 @@ import './Header.css';
 const Header = () => {
     return (
         <>
-            <Navbar bg="white" className='border' sticky="top">
+            <Navbar collapseOnSelect expand="lg" bg="white" className='border' sticky="top">
                 <Container>
                     <Navbar.Brand as={Link} to="/home#home">
                         <img className='width' src="https://i.ibb.co/d2Nt3GJ/favicon-1.jpg" alt="" />
                     </Navbar.Brand>
-                    <Nav className="me-auto">
+                    <Navbar.Toggle />
+                    <Navbar.Collapse className="justify-content-end">
                         <Nav.Link as={Link} to="/home#home">Home</Nav.Link>
                         <Nav.Link as={Link} to="/home#services">Services</Nav.Link>
                         <Nav.Link as={Link} to="/home#about">About</Nav.Link>
                         <Nav.Link as={Link} to="/Contact">Contact</Nav.Link>
                         <Nav.Link as={Link} to="/Login">Log In</Nav.Link>
-                    </Nav>
-                    <Navbar.Collapse className="justify-content-end">
                         <Navbar.Text>
                             Signed in as: <a href="#Login">Jack</a>
                         </Navbar.Text>
